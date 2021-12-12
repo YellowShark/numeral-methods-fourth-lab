bf9255cf63cf1f8659dd5151999cc1e14e5f0e5f
Daniil <danil.dobrodeev@mail.ru>
2021-12-12 19:46:25 +0300

    final implementation

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,36 @@
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
 import java.util.*
 
-const val N = 5
+const val INPUT_FILE = "in.txt"
+const val OUTPUT_FILE = "out.txt"
 
 fun main() {
-    val s = Scanner(System.`in`)
+    fun writeErrorInTheFile(errorMessage: String) {
+        File(OUTPUT_FILE).bufferedWriter().use { out ->
+            out.write(errorMessage)
+        }
+    }
+
+    val s = Scanner(File(INPUT_FILE))
     println("Функция f(x) = 1 / ln (x)")
-    println("Введите границы интегрирования:")
-    println("a =")
     val a = s.nextFloat()
-    println("b =")
     val b = s.nextFloat()
-    println("Введите точность вычисления")
-    val eps = s.nextDouble()
-    TrapeziumMethod().run {
-        calculate(a = a, b = b, n = N, eps = eps)
+    val eps = s.nextFloat()
+    val n = s.nextInt()
+    val hMin = s.nextFloat()
+    println("Границы интегрирования:")
+    println("a = $a")
+    println("b = $b")
+    println("Точность вычисления: $eps")
+    println("Начальное число частичных отрезков: $n")
+    println("Наименьшее допустимое значение шага интегрирования: $hMin")
+    println("------------------------------------------------------------------")
+
+    TrapeziumMethod().runCatching {
+        calculate(a = a, b = b, n = n, eps = eps, hMin = hMin)
+    }.onFailure { t ->
+        t.message?.let { writeErrorInTheFile(it) }
     }
 }
--- src/TrapeziumMethod.kt
+++ src/TrapeziumMethod.kt
@@ -1,4 +1,6 @@
-import java.lang.Math.pow
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
 import kotlin.math.abs
 import kotlin.math.ln
 import kotlin.math.pow
@@ -6,22 +8,36 @@ import kotlin.math.pow
 const val S = 2f
 
 class TrapeziumMethod {
-    fun calculate(a: Float, b: Float, n: Int, eps: Double) {
+    fun calculate(a: Float, b: Float, n: Int, eps: Float, hMin: Float) {
+        if (a >= b)
+            throw ArithmeticException("IER = 3. Ошибка входных данных.")
         var e: Float
-        var prevIntegral = calc(a, b, n)
+        var prevIntegral = calc(a, b, n, hMin)
         var newN = n
         do {
             newN *= 2
-            val currIntegral = calc(a, b, newN)
+            val currIntegral = calc(a, b, newN, hMin)
+            if (currIntegral >= prevIntegral)
+                throw ArithmeticException("IER = 1. Решение не получено, т.к. погрешность перестала уменьшаться.")
             e = abs((prevIntegral - currIntegral) / ( (0.5f).pow(S) - 1) )
             prevIntegral = currIntegral
         } while (e > eps)
-        println("Точность $eps достигнута $newN разбиениями.")
-        println("Интеграл равен $prevIntegral.")
+
+        writeResultInTheFile(e, eps, newN, a, b, prevIntegral)
     }
 
-    private fun calc(a: Float, b: Float, n: Int): Float {
+    private fun writeResultInTheFile(e: Float, eps: Float, newN: Int, a: Float, b: Float, result: Float) {
+        File(OUTPUT_FILE).bufferedWriter().use { out ->
+            out.write("IER = 0. Решение получено.\n")
+            out.write("Точность $e <= $eps достигнута $newN разбиениями (шагом ${(b - a) / newN}).\n")
+            out.write("Интеграл равен $result.\n")
+        }
+    }
+
+    private fun calc(a: Float, b: Float, n: Int, hMin: Float): Float {
         val h = (b - a) / n
+        if (h < hMin)
+            throw ArithmeticException("IER = 2. Шаг интегррования стал слишком мал.")
         var x = a
         var f = (f(x) + f(b)) / 2
         for (i in 1 until n) {
